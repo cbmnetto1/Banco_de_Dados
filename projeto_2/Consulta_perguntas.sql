b1e79c3d3e6f579b9f6c1ef19d9dd558132bb42b
Celso Netto <celso.bmnetto1@gmail.com>
2024-11-24 18:17:53 -0300

    consultas e perguntas em um so arquivo

--- projeto_2/Consulta_perguntas.sql
+++ projeto_2/Consulta_perguntas.sql
@@ -1,5 +1,4 @@
 -- 1 pergunta
-
 SELECT f.fk_Pessoa_ID AS Funcionario_ID, p.Nome, COUNT(t.fk_Evento_ID_Evento) AS Qtd_Eventos
 FROM Funcionario f
 JOIN Pessoa p ON f.fk_Pessoa_ID = p.ID
@@ -27,7 +26,6 @@ GROUP BY Hora
 ORDER BY Total_Consultas DESC;
 
 -- 4 pergunta
-
 SELECT p.Nome, COUNT(pe.ID_Evento) AS Total_Eventos
 FROM Patrocinador p
 JOIN Patrocina pe ON pe.fk_Patrocina_ID_Patrocinador = p.ID_Patrocinador
@@ -39,3 +37,89 @@ LIMIT 1;
 SELECT Nome, Preco
 FROM Ingrediente
 ORDER BY Preco DESC;
+
+-- 6 pergunta
+SELECT 
+    i.Nome AS Nome_Item,
+    ing.Nome AS Nome_Ingrediente,
+    SUM(ing.Quantidade) AS Quantidade_Total_Utilizada,
+    SUM(ing.Quantidade * ing.Preco) AS Custo_Total_Ingrediente
+FROM 
+    Contem_Item ci
+JOIN 
+    Pedido p ON ci.fk_Pedido_ID_Pedido = p.ID_Pedido
+JOIN 
+    Item i ON ci.fk_Item_ID_Item = i.ID_Item
+JOIN 
+    Contem_Ingrediente ci_ing ON i.ID_Item = ci_ing.fk_Item_ID_Item
+JOIN 
+    Ingrediente ing ON ci_ing.fk_Ingrediente_ID_Ingrediente = ing.ID_Ingrediente
+GROUP BY 
+    i.ID_Item, i.Nome, ing.Nome
+ORDER BY 
+    SUM(p.ID_Pedido) DESC, Custo_Total_Ingrediente DESC;
+
+-- 7 pergunta
+SELECT 
+    conv.nome AS Nome_Convenio,
+    COUNT(cons.ID_Consulta) AS Frequencia_Visitas,
+    AVG(itens.preco * itens.Quantidade_Item) AS Custo_Medio_Atendimento
+FROM 
+    Consulta cons
+JOIN 
+    Pessoa p ON cons.fk_Pessoa_ID_pessoa = p.ID_pessoa
+JOIN 
+    Convenio conv ON p.fk_Convenio_ID_Convenio = conv.ID_Convenio
+LEFT JOIN 
+    Pedido ped ON ped.fk_Pessoa_ID_pessoa = p.ID_pessoa
+LEFT JOIN 
+    Item itens ON itens.fk_Pedido_ID_Pedido = ped.ID_Pedido
+GROUP BY 
+    conv.nome
+ORDER BY 
+    Frequencia_Visitas DESC;
+
+-- 8 pergunta
+SELECT 
+    m.numero AS Numero_Mesa,
+    COUNT(r.ID_Reserva) AS Total_Reservas_Eventos
+FROM 
+    Reserva r
+JOIN 
+    Mesa m ON r.ID_Mesa = m.ID_Mesa
+JOIN 
+    Participa p ON r.fk_Pessoa_ID_pessoa = p.fk_Pessoa_ID_pessoa
+JOIN 
+    Evento e ON p.fk_Evento_ID_Evento = e.ID_Evento
+GROUP BY 
+    m.numero
+ORDER BY 
+    Total_Reservas_Eventos DESC;
+
+-- 9 pergunta
+SELECT 
+    e.Local AS Tipo_Evento,
+    SUM(i.preco * i.Quantidade_Item) AS Receita_Gerada
+FROM 
+    Evento e
+JOIN 
+    Participa p ON e.ID_Evento = p.fk_Evento_ID_Evento
+JOIN 
+    Pedido ped ON p.fk_Pessoa_ID_pessoa = ped.fk_Pessoa_ID_pessoa
+JOIN 
+    Item i ON i.fk_Pedido_ID_Pedido = ped.ID_Pedido
+GROUP BY 
+    e.Local
+ORDER BY 
+    Receita_Gerada DESC;
+
+-- 10 pergunta
+SELECT 
+    r.Medicamento AS Nome_Medicamento,
+    COUNT(r.ID_Receita) AS Total_Prescricoes
+FROM 
+    Receita r
+GROUP BY 
+    r.Medicamento
+ORDER BY 
+    Total_Prescricoes DESC;
